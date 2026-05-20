677e308aa8832420c864257226a0dc50f26d9ae7
qin_xs <qin_xs@qq.com>
2026-05-20 21:39:23 +0800

    修正 窗口居中

--- main.py
+++ main.py
@@ -38,19 +38,23 @@ async def setup_window(page: ft.Page, app: App) -> None:
 
     if not page.web:
         try:
-            await page.window.center()
-            await page.window.to_front()
             if app.settings.user_config.get("remember_window_size"):
                 window_width = app.settings.user_config.get("window_width")
                 window_height = app.settings.user_config.get("window_height")
                 if window_width and window_height:
                     page.window.width = int(window_width)
                     page.window.height = int(window_height)
+                    page.update()
+                    await page.window.center()
+                    await page.window.to_front()
                     return
 
             screen = get_monitors()[0]
             page.window.width = int(screen.width * WINDOW_SCALE)
             page.window.height = int(screen.height * WINDOW_SCALE)
+            page.update()
+            await page.window.center()
+            await page.window.to_front()
         except IndexError:
             logger.warning("No monitors detected, using default window size.")
 
